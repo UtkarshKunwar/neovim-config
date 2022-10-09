bab87c3da64e1339b0f33232fd3fc23e5f721359
Utkarsh Kunwar <14164924+UtkarshKunwar@users.noreply.github.com>
2022-10-09 19:58:38 +0530

    feat(plugins): added completion

--- lua/packer_init.lua
+++ lua/packer_init.lua
@@ -9,7 +9,6 @@
 -- neovim-lua/README.md
 -- https://github.com/brainfucksec/neovim-lua#readme
 
-
 -- Automatically install packer
 local fn = vim.fn
 local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
@@ -58,6 +57,17 @@ return packer.startup(function(use)
   use {"andymass/vim-matchup", event="VimEnter"} -- Extend % support for matching keywords
   use "flazz/vim-colorschemes" -- All the colorschemes
   use "Mofiqul/vscode.nvim" -- Look like VSCode for the normies
+
+  -- cmp plugins
+  use "hrsh7th/nvim-cmp" -- The completion plugin
+  use "hrsh7th/cmp-buffer" -- buffer completions
+  use "hrsh7th/cmp-path" -- path completions
+  use "hrsh7th/cmp-cmdline" -- cmdline completions
+  use "saadparwaiz1/cmp_luasnip" -- snippet completions
+
+  -- snippets
+  use "L3MON4D3/LuaSnip" --snippet engine
+  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
