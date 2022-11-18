508085be737a34da68dbc8c05bda951b8be9dab1
Utkarsh Kunwar <14164924+UtkarshKunwar@users.noreply.github.com>
2022-11-18 17:52:04 +0530

    refactor(keymaps): adjusted keymaps for telescope

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -33,7 +33,6 @@ keymap("n", "<leader>f", ":Lex 30<cr>", opts)
 
 -- Tabs --
 keymap("n", "<C-t>", ":tabnew <bar> :NERDTreeMirror <bar> wincmd p <CR>", opts)
-keymap("n", "<C-x>", ":tabclose<CR>", opts)
 
 -- Navigation that just makes sense to me --
 keymap("n", "H", "^", opts)
@@ -89,4 +88,4 @@ keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
 -- Plugins --
 -- Telescope --
 keymap("n", "<C-f>", "<cmd>lua require'telescope.builtin'.find_files()<CR>", opts)
-keymap("n", "<C-G>", "<cmd>lua require'telescope.builtin'.live_grep()<CR>", opts)
+keymap("n", "<C-A-f>", "<cmd>lua require'telescope.builtin'.live_grep()<CR>", opts)
