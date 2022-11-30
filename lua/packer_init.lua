0528866bdb034a62f51a9a71a12f540fd3813e72
Utkarsh Kunwar <14164924+UtkarshKunwar@users.noreply.github.com>
2022-11-30 17:26:46 +0530

    feat: added gitgutter-like gitsigns

--- lua/packer_init.lua
+++ lua/packer_init.lua
@@ -104,6 +104,9 @@ return packer.startup(function(use)
   use "numToStr/Comment.nvim" -- allows commenting
   use "JoosepAlviste/nvim-ts-context-commentstring" -- Uses treesitter to get context for better commenting
 
+  -- Gitgutter
+  use "lewis6991/gitsigns.nvim"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
