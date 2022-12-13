e394ae455437846def84fb49b6eacdc08a13a639
Utkarsh Kunwar <14164924+UtkarshKunwar@users.noreply.github.com>
2022-12-13 15:08:41 +0530

    fix(rainbow): custom colors and glitchy rainbow

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -7,19 +7,19 @@ configs.setup {
     ensure_installed = "all", -- one of "all" or a list of languages
     sync_install = false, -- install languages asynchronously
     auto_install = true, -- installs missing parsers on opening file
-    ignore_install = {""}, -- list of languages to ignore installing
+    ignore_install = { "" }, -- list of languages to ignore installing
     highlight = {
         enable = true, -- false disables whole extension
-        disable = {""}, -- list of languages to be disabled for highlight
+        disable = { "" }, -- list of languages to be disabled for highlight
         additional_vim_regex_highlighting = true,
     },
-    indent = {enable = true, disable = {"yaml"}},
+    indent = { enable = true, disable = { "yaml" } },
     rainbow = {
         enable = true,
         -- disable = {"cpp"}, -- list of languages to be disabled
         extended_mode = true,
         max_file_lines = nil, -- Do not enable for files with more than n lines, int
-        -- colors = {}, -- table of hex strings,
+        colors = { 'hotpink3', 'cyan', 'mediumpurple', 'yellow' }, -- table of hex strings,
         -- termcolors = {}, -- table of colour name strings
     },
     context_commentstring = {
@@ -29,3 +29,9 @@ configs.setup {
 }
 
 require('hlargs').setup()
+
+-- HACK: for glitchy rainbow https://github.com/p00f/nvim-ts-rainbow/issues/112#issuecomment-1310835936
+vim.api.nvim_command([[
+    autocmd BufNewFile,BufReadPost * TSDisable rainbow | TSEnable rainbow | TSDisable rainbow | TSEnable rainbow | TSDisable rainbow | TSEnable rainbow
+    autocmd BufWritePost * TSDisable rainbow | TSEnable rainbow
+]])
