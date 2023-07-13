20886c2d70259230852cf937bdd2e09d4c8803c9
Utkarsh Kunwar <14164924+UtkarshKunwar@users.noreply.github.com>
2023-07-13 14:36:56 +0530

    feat(plugins): added color visualisation support

--- lua/packer_init.lua
+++ lua/packer_init.lua
@@ -177,6 +177,9 @@ return packer.startup(function(use)
     use("theHamsta/nvim-dap-virtual-text")
     use("nvim-telescope/telescope-dap.nvim")
 
+    -- All 'bout dat colour code vizz
+    use("norcalli/nvim-colorizer.lua")
+
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if packer_bootstrap then
