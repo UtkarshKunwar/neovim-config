dae87ef18a18bb39e086ab36abbc6d42ec0ad425
Utkarsh Kunwar <14164924+UtkarshKunwar@users.noreply.github.com>
2023-12-25 03:19:14 +0530

    fix(neovide): changed default scaling factor

--- lua/core/neovide.lua
+++ lua/core/neovide.lua
@@ -8,7 +8,7 @@ if vim.g.neovide then
     keymap("i", "<D-v>", '<ESC>l"+Pli', opts) -- Paste insert mode
 
     -- Dynamic scaling of the display
-    vim.g.neovide_scale_factor = 1.0
+    vim.g.neovide_scale_factor = 0.95
     local change_scale_factor = function(delta)
         vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
     end
