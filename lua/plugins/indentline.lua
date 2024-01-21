536ecb447203c426bb7f6d4b44570f4f3ebe63b4
Utkarsh Kunwar <14164924+UtkarshKunwar@users.noreply.github.com>
2024-01-22 00:25:53 +0530

    refactor(ibl): make scope hl less distracting from rainbow

--- lua/plugins/indentline.lua
+++ lua/plugins/indentline.lua
@@ -17,18 +17,20 @@ local highlight = {
     "RainbowViolet",
     "RainbowCyan",
 }
-local hooks = require("ibl.hooks")
+
 -- create the highlight groups in the highlight setup hook, so they are reset
 -- every time the colorscheme changes
-hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-    vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-    vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-    vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-    vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-    vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-    vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-    vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
-end)
+-- Uncomment to use rainbow delimiters colours for ibl
+-- local hooks = require("ibl.hooks")
+-- hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+--     vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+--     vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+--     vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+--     vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+--     vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+--     vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+--     vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+-- end)
 
 -- Not using setup() call because of additional overhead according to the documentation.
 vim.g.rainbow_delimiters = {
@@ -64,11 +66,13 @@ indent_blankline.setup({
     scope = {
         enabled = true,
         char = "▏",
-        highlight = highlight,
+        -- highlight = highlight, -- change to highlight from rainbow delim for rainbow ibl
+        highlight = "Conceal", -- change to highlight from rainbow delim for rainbow ibl
         -- include = {
         --     node_type = { ["*"] = { "*" } },
         -- },
     },
 })
 
-hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+-- Uncomment to use rainbow delimiters colours for ibl
+-- hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
