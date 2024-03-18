de7aca0616786143e8666cdac601573b74fcbdb9
Utkarsh Kunwar <14164924+UtkarshKunwar@users.noreply.github.com>
2024-03-18 19:25:50 +0530

    fix(ftplugin): removed mandatory tabs for md

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -8,10 +8,3 @@ vim.b.markdown_ftplugin = true
 
 vim.opt.wrap = true
 vim.opt.textwidth = 80
-
--- Set tab and indentation behavior specifically for Markdown files
--- This is based on how mdformat formats the Markdown files.
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
