6d1d9f91ebfcbfbb562e1948f5b424cccbfeb2eb
Utkarsh Kunwar <14164924+UtkarshKunwar@users.noreply.github.com>
2024-03-22 02:45:59 +0530

    fix(plugin): fix incorrect rainbow-delimiter configuration

--- lua/plugins/indentline.lua
+++ lua/plugins/indentline.lua
@@ -22,16 +22,16 @@ local highlight = {
 -- create the highlight groups in the highlight setup hook, so they are reset
 -- every time the colorscheme changes
 -- Uncomment to use rainbow delimiters colours for ibl
--- local hooks = require("ibl.hooks")
--- hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
---     vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
---     vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
---     vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
---     vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
---     vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
---     vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
---     vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
--- end)
+local hooks = require("ibl.hooks")
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+    vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+    vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+    vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+    vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+    vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+    vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+    vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+end)
 
 -- Not using setup() call because of additional overhead according to the documentation.
 vim.g.rainbow_delimiters = {
