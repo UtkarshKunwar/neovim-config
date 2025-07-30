a755157702ac937bbb89ff4825db6592d9dd3d82
Utkarsh Kunwar <14164924+UtkarshKunwar@users.noreply.github.com>
2025-07-30 22:50:41 +0530

    feat(plugin): added leetcode plugin

--- lua/lazy_init.lua
+++ lua/lazy_init.lua
@@ -247,4 +247,18 @@ lazy.setup({
             })
         end,
     },
+
+    -- 1337 anyone?
+    {
+        "kawre/leetcode.nvim",
+        build = ":TSUpdate html", -- if you have `nvim-treesitter` installed
+        dependencies = {
+            -- include a picker of your choice, see picker section for more details
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+        },
+        opts = {
+            -- configuration goes here
+        },
+    },
 })
